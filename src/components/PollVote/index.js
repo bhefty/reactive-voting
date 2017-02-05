79c406e404c3497b1a9a1d495864311f799e4fc0
Bill Hefty <billhefty@gmail.com>
2017-02-04 22:14:32 -0600

    List options as radio buttons

--- src/components/PollVote/index.js
+++ src/components/PollVote/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import Paper from 'material-ui/Paper'
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class PollVote extends Component {
   constructor() {
     super()
     this.state = {
-      poll: {}
+      poll: {},
+      selected: ''
     }
     this.getPollInfo = this.getPollInfo.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
@@ -24,10 +29,46 @@ export default class PollVote extends Component {
       .then((poll) => this.setState({poll: poll}))
   }
 
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log('Submit', this.state.selected)
+  }
+
   render() {
+    const styles = {
+      submitButtonStyle: {
+        margin: '20px auto',
+        display: 'block',
+        maxWidth: '10em'
+      }
+    }
+    let renderOptions
+    if (!this.state.poll.options) {
+      renderOptions = (<RadioButton />)
+    } else {
+      renderOptions = this.state.poll.options.map((option, idx) => {
+        return (
+          <RadioButton
+            key={idx}
+            value={option.choice}
+            label={option.choice}
+            onClick={() => this.setState({selected: option._id})}
+          />
+        )
+      })
+    }
     return (
       <div>
         <h2>{this.state.poll.title}</h2>
+        <p>by: {this.state.poll.author}</p>
+        <Paper zDepth={1} rounded={false}>
+          <form onSubmit={this.handleSubmit}>
+            <RadioButtonGroup name='pollOptions'>
+              {renderOptions}
+            </RadioButtonGroup>
+            <RaisedButton type='submit' style={styles.submitButtonStyle} backgroundColor='#58B957' labelColor='#fff' label='Submit' />
+          </form>
+        </Paper>
       </div>
     )
   }
