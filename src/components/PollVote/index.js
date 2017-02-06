fed0f087a959f1567ec5c737531d4b45f6dd52d7
Bill Hefty <billhefty@gmail.com>
2017-02-06 11:44:20 -0600

    Add react-chartjs library

--- src/components/PollVote/index.js
+++ src/components/PollVote/index.js
@@ -3,6 +3,8 @@ import Paper from 'material-ui/Paper'
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
 import RaisedButton from 'material-ui/RaisedButton'
 
+import VoteChart from '../VoteChart'
+
 export default class PollVote extends Component {
   constructor() {
     super()
@@ -35,13 +37,21 @@ export default class PollVote extends Component {
   }
 
   render() {
-    const styles = {
-      submitButtonStyle: {
-        margin: '20px auto',
-        display: 'block',
-        maxWidth: '10em'
-      }
-    }
+    const chartData = [
+          {
+              value: 5,
+              color:"#F7464A",
+              highlight: "#FF5A5E",
+              label: "CUID"
+          },
+          {
+              value: 2,
+              color: "#46BFBD",
+              highlight: "#5AD3D1",
+              label: "ObjectID"
+          },
+    ]
+
     let renderOptions
     if (!this.state.poll.options) {
       renderOptions = (<RadioButton />)
@@ -61,13 +71,15 @@ export default class PollVote extends Component {
       <div>
         <h2>{this.state.poll.title}</h2>
         <p>by: {this.state.poll.author}</p>
-        <Paper zDepth={1} rounded={false}>
+        <Paper zDepth={2}>
           <form onSubmit={this.handleSubmit}>
+            <h4>Cast your vote:</h4>
             <RadioButtonGroup name='pollOptions'>
               {renderOptions}
             </RadioButtonGroup>
-            <RaisedButton type='submit' style={styles.submitButtonStyle} backgroundColor='#58B957' labelColor='#fff' label='Submit' />
+            <RaisedButton type='submit' backgroundColor='#58B957' labelColor='#fff' label='Submit' />
           </form>
+          <VoteChart data={chartData} />
         </Paper>
       </div>
     )
